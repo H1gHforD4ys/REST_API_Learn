cf3117402b046043f5c188c3fc652fd4e17c86ac
Kite170901 <varussvg@gmail.com>
2023-07-08 14:46:52 +0700

    migrate

--- database/migrations/2023_07_08_074048_create_customer.php
+++ database/migrations/2023_07_08_074048_create_customer.php
@@ -11,13 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('customer', function (Blueprint $table) {
+            $table->bigIncrements('id_customer');
+            $table->string('name_customer');
+            $table->string('phone_customer');
+            $table->string('address_customer');
+            $table->string('email_customer');
+            $table->string('city_customer');
+
             $table->timestamps();
         });
     }
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('customer');
     }
 };
